3f793d462cc9277d0503dd08d4a84bfb2d304753
Sergey <sergey.mikhalkin.work@gmail.com>
2023-06-06 23:41:33 +0300

    feat: add bank type

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,5 @@
 export type { SignUpFormData } from './types/SignUpFormData';
 export type { SignInFormData } from './types/SignInFormData';
+export type { Bank } from './types/Bank';
 export { useLocalStorage } from './hooks/useLocalStorage';
 export { LS_KEYS } from './constants/constants';
---
+++ src/shared/types/Bank.ts
@@ -0,0 +1,13 @@
+type StringArray = string;
+
+export type Bank = {
+  filial_id: string;
+  sap_id: string;
+  info_worktime: StringArray;
+  street_type: string;
+  street: string;
+  filials_text: string;
+  home_number: string;
+  name: string;
+  name_type: string;
+};
