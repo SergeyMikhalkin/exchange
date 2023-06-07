0bcb26f4173b75f9dcaff0c3371a73623e615f34
Sergey <sergey.mikhalkin.work@gmail.com>
2023-06-07 22:26:16 +0300

    feat: add interaction with the store(main page)

--- src/pages/main/ui/Page/Page.tsx
+++ src/pages/main/ui/Page/Page.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBanks, getBanks, getStatus } from 'app/redux/banksSlice';
+import { AppDispatch } from 'app/redux/store';
+import BanksList from 'widgets/BanksList';
 
 const { Title } = Typography;
 
 export function MainPage() {
-  return <Title>Main</Title>;
+  const dispatch = useDispatch<AppDispatch>();
+  const banks = useSelector(getBanks);
+  const banksStatus = useSelector(getStatus);
+
+  useEffect(() => {
+    if (banksStatus === 'idle') {
+      dispatch(fetchBanks());
+    }
+  }, [banksStatus, dispatch]);
+
+  return (
+    <>
+      <Title>Banks</Title>
+      <BanksList banks={banks} />
+    </>
+  );
 }
