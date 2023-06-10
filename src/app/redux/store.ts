20fbc84a1ca3d996638fe8a41166f9544fbdaa11
Sergey <sergey.mikhalkin.work@gmail.com>
2023-06-10 12:14:18 +0300

    feat: add middleware for history of searching

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import banksSlice from './banksSlice';
+import { saveHistoryMiddleware } from './saveHistoryMiddleware';
 
 const store = configureStore({
   reducer: {
     banks: banksSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saveHistoryMiddleware),
 });
 
 export default store;
